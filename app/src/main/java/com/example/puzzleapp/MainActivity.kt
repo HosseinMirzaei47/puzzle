9f9623452bac022e49dcf508f836fb9f0d62352e
hossein <hosseinhidd@gmail.com>
2020-11-12 20:36:54 +0330

    UI improvement in fragment_level.xml and fragment_puzzle_game.xml.

--- app/src/main/java/com/example/puzzleapp/MainActivity.kt
+++ app/src/main/java/com/example/puzzleapp/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.puzzleapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.puzzleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        hideSystemUI()
+        return super.onCreateView(name, context, attrs)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun hideSystemUI() {
+        window?.decorView?.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
